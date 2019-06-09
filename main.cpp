80b781a4caf7a1b493213d983dbb417614251eb7
Ian Davis <idtx314business@gmail.com>
2019-06-08 23:42:28 -0500

    I've gotten the most basic functions of the library working. It's importing and the struct is operating as expected. I intend to expand this struct to contain all the appropriate data for a matrix in order to make matrices easier to pass around. I may ultimately include member functions for matrix operations as well.

--- main.cpp
+++ main.cpp
@@ -4,28 +4,31 @@ license
 */
 
 // Includes
+#include <iostream>
+#include "linalg.h"
 
 
 // Global Variables
 
 
 // Demonstrator function
-void main():
+int main(void)
 {
-
     // Local Variables
+    matrix m;
 
 
     // Matrix Composition
+    m.height = 3;
+    m.width = 5;
 
 
 
 
 
     // Header Demonstration
+    std::cout << m.height << " " << m.width << std::endl;
 
 
-
-
-    return;
+    return 0;
 }
