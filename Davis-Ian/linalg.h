4d43e664d76785331666049ceb844c1214c26ab8
Ian Davis <idtx314business@gmail.com>
2019-06-26 15:04:04 -0500

    Made deployment manual package instructions slightly more specific.

--- Davis-Ian/linalg.h
+++ Davis-Ian/linalg.h
@@ -140,4 +140,182 @@ inline matrix matrix_transpose(matrix input){
 }
 
 
+// This function performs a series of checks to make sure that the library is working properly. The results of specific tests are printed to standard out. The return value is the number of tests failed.
+inline int unit_tests(){
+
+    // Initialize variables
+    bool pass;
+    int error_count = 0;
+    int height1 = 3;
+    int height2 = 2;
+    int height3 = 4;
+    int height_2x3 = 2;
+    int width1 = 5;
+    int width2 = 4;
+    int width3 = 3;
+    int width_2x3 = 3;
+    std::vector<double> values1{1,2,3,4,5,6,7,8,9,10,11,12,13,14,15};
+    std::vector<double> values2{1,1,1,1,2,2,2,2};
+    std::vector<double> values3{1,2,3,1,2,3,1,2,3,1,2,3};
+    std::vector<double> values_2x3{4,8,12,8,16,24};
+    std::vector<double> values1t{1,6,11,2,7,12,3,8,13,4,9,14,5,10,15};
+
+
+    // Test Matrix Initialization
+    // Initialize Undersize Matrix
+    pass = false;
+    try{
+        matrix m1(values3, height2, width2);
+    }
+    catch(std::string e){
+        if(e == "Error: Vector size does not match given matrix dimensions. Aborting variable creation"){pass = true;}
+    }
+
+    if(pass){
+        std::cout << "Unit Test Passed: Initialize Undersize Matrix" << std::endl;
+    }
+    else{
+        std::cout << "Unit Test Failed: Initialize Undersize Matrix" << std::endl;
+        error_count += 1;
+    }
+
+    // Initialize Oversize Matrix
+    pass = false;
+    try{
+        matrix m2(values1, height2, width2);
+    }
+    catch(std::string e){
+        if(e == "Error: Vector size does not match given matrix dimensions. Aborting variable creation"){pass = true;}
+    }
+    if(pass){
+        std::cout << "Unit Test Passed: Initialize Oversize Matrix" << std::endl;
+    }
+    else{
+        std::cout << "Unit Test Failed: Initialize Oversize Matrix" << std::endl;
+        error_count += 1;
+    }
+
+    // Initialize Null And Empty Matrix
+    pass = false;
+    try{
+        matrix m3(std::vector<double>(),NULL,NULL);
+        matrix m4;
+
+        if (m3.values()==m4.values() && m3.height()==m4.height() && m3.width()==m4.width()){pass = true;}
+    }
+    catch(std::string e){}
+    if(pass){
+        std::cout << "Unit Test Passed: Initialize NULL And Empty Matrix" << std::endl;
+    }
+    else{
+        std::cout << "Unit Test Failed: Initialize NULL And Empty Matrix" << std::endl;
+        error_count += 1;
+    }
+
+    // Initialize Normal Matrix
+    pass = false;
+    try{
+        matrix m5(values1,height1,width1);
+        if (m5.values()==values1 && m5.height() == height1 && m5.width() == width1){pass = true;}
+    }
+    catch(std::string e){}
+    if(pass){
+        std::cout << "Unit Test Passed: Initialize Normal Matrix" << std::endl;
+    }
+    else{
+        std::cout << "Unit Test Failed: Initialize Normal Matrix" << std::endl;
+        error_count += 1;
+    }
+
+    // Test Matrix Multiplication
+    // Test Mismatched Input
+    pass = false;
+    try{
+        matrix m6(values1, height1, width1);
+        matrix m7(values2, height2, width2);
+        matrix m8 = matrix_multiply(m6,m7);
+    }
+    catch(std::string e){
+        if(e == "Error: Width of input matrix 1 does not match height of input matrix 2. Multiplication is not defined!"){pass = true;}
+    }
+    if(pass){
+        std::cout << "Unit Test Passed: Multiply Mismatched Input" << std::endl;
+    }
+    else{
+        std::cout << "Unit Test Failed: Multiply Mismatched Input" << std::endl;
+        error_count += 1;
+    }
+
+    // Test Null Input
+    pass = false;
+    try{
+        matrix m9;
+        matrix m10;
+        matrix m11 = matrix_multiply(m9,m10);
+        if(m11.values() == std::vector<double>() && m11.height() == 0 && m11.width() == 0){pass = true;}
+    }
+    catch(std::string e){}
+    if(pass){
+        std::cout << "Unit Test Passed: Multiply NULL Input" << std::endl;
+    }
+    else{
+        std::cout << "Unit Test Failed: Multiply NULL Input" << std::endl;
+        error_count += 1;
+    }
+
+    // Test Valid Input
+    pass = false;
+    try{
+        matrix m12(values2, height2, width2);
+        matrix m13(values3, height3, width3);
+        matrix m14 = matrix_multiply(m12,m13);
+        if(m14.values() == values_2x3 && m14.height() == height_2x3 && m14.width() == width_2x3){pass = true;}
+    }
+    catch(std::string e){}
+    if(pass){
+        std::cout << "Unit Test Passed: Multiply Valid Input" << std::endl;
+    }
+    else{
+        std::cout << "Unit Test Failed: Multiply Valid Input" << std::endl;
+        error_count += 1;
+    }
+
+    // Test Matrix Transposition
+    // Test NULL Input
+    pass = false;
+    try{
+        matrix m15;
+        matrix m16 = matrix_transpose(m15);
+        if(m16.values() == std::vector<double>() && m16.height() == 0 && m16.width() == 0){pass = true;}
+    }
+    catch(std::string e){}
+    if(pass){
+        std::cout << "Unit Test Passed: Transpose NULL Input" << std::endl;
+    }
+    else{
+        std::cout << "Unit Test Failed: Transpose NULL Input" << std::endl;
+        error_count += 1;
+    }
+
+    // Test Normal Input
+    pass = false;
+    try{
+        matrix m17(values1, height1, width1);
+        matrix m18 = matrix_transpose(m17);
+        if(m18.values() == values1t && m18.height() == width1 && m18.width() == height1){pass = true;}
+    }
+    catch(std::string e){}
+    if(pass){
+        std::cout << "Unit Test Passed: Transpose Normal Input" << std::endl;
+    }
+    else{
+        std::cout << "Unit Test Failed: Transpose Normal Input" << std::endl;
+        error_count += 1;
+    }
+
+
+    return error_count;
+}
+
+
 #endif /* LALG_H */
